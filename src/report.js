3851e726170ede96098a4c4e2e9ebac1fcb392f0
A Fox <arthur.fox@hotmail.co.uk>
2021-12-28 21:36:07 -0300

    Added Attribute count to report

--- src/report.js
+++ src/report.js
@@ -12,18 +12,26 @@ const reportMetaData = _metadataFile => {
 
     let resultMap = new Map();
     layersOrder.forEach( (layer, _index) => {
-      resultMap.set(layer.name, Array(rarity.length).fill(0)) 
+      resultMap.set(layer.name, Array(rarity.length).fill(0))
     })
+    resultMap.set("Attributes", Array(layersOrder.length).fill(0))
     
     jsonParsed.forEach( (item, _itemIndex) => {
+
+      if (_itemIndex >= 4420) return;
+
+      let arr = resultMap.get("Attributes");
+      arr[item.attributes[0].value] += 1;
+      resultMap.set("Attributes", arr);      
+
       item.attributes.forEach( (attr, _attrIndex) => {        
         for(let i = 0; i < rarity.length; i++)
         {
           if (attr.rarity == rarity[i].val)
           {
-            let arr = resultMap.get(attr.layer);
+            let arr = resultMap.get(attr.trait_type);
             arr[i] +=1;
-            resultMap.set(attr.layer, arr);
+            resultMap.set(attr.trait_type, arr);
             break;
           }
         }
@@ -32,12 +40,20 @@ const reportMetaData = _metadataFile => {
 
     resultMap.forEach( (value, key) => {
       console.log(`-------${key.toUpperCase()}-------`);
-      for(let i = 0; i < rarity.length; i++) {
-        console.log(`${rarity[i].val}: ${value[i]}`);
+      if (key == "Attributes")
+      {
+        for(let i = 0; i < layersOrder.length; i++) {
+          console.log(`${i+1}: ${value[i]}`);
+        }
       }
+      else
+      {
+        for(let i = 0; i < rarity.length; i++) {
+          console.log(`${rarity[i].val}: ${value[i]}`);
+        }
+      }      
       console.log('');
     });
-
   });
 };
 
